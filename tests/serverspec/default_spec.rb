b71ebd071f7dfa8af94a72d89768144874f7a1af
Tomoyuki Sakurai <y@trombik.org>
2020-04-06 15:16:33 +0900

    initial implementation

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,20 +1,21 @@
 require "spec_helper"
 require "serverspec"
 
-package = "mariadb"
-service = "mariadb"
-config  = "/etc/mariadb/mariadb.conf"
-user    = "mariadb"
-group   = "mariadb"
-ports   = [PORTS]
-log_dir = "/var/log/mariadb"
-db_dir  = "/var/lib/mariadb"
+package = "mysql"
+service = "mysql-server"
+config_dir = "/etc/mysql"
+user    = "mysql"
+group   = "mysql"
+ports   = [3306]
+db_dir  = "/var/lib/mysql"
 
 case os[:family]
 when "freebsd"
-  config = "/usr/local/etc/mariadb.conf"
-  db_dir = "/var/db/mariadb"
+  package = "databases/mysql57-server"
+  config_dir = "/usr/local/etc/mysql"
+  db_dir = "/var/db/mysql"
 end
+config  = "#{config_dir}/my.cnf"
 
 describe package(package) do
   it { should be_installed }
@@ -22,27 +23,14 @@ end
 
 describe file(config) do
   it { should be_file }
-  its(:content) { should match Regexp.escape("mariadb") }
-end
-
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
+  its(:content) { should match Regexp.escape("Managed by ansible") }
 end
 
 case os[:family]
 when "freebsd"
-  describe file("/etc/rc.conf.d/mariadb") do
+  describe file("/etc/rc.conf.d/#{service.gsub('-', '_')}") do
     it { should be_file }
+    its(:content) { should match Regexp.escape("Managed by ansible") }
   end
 end
 
@@ -55,4 +43,23 @@ ports.each do |p|
   describe port(p) do
     it { should be_listening }
   end
+end
+
+describe file "/root/.my.cnf" do
+  it { should exist }
+  it { should be_file }
+  it { should be_owned_by "root" }
+  it { should be_mode 600 }
+  its(:content) { should match(/Managed by ansible/) }
+  its(:content) { should match(/^password = "PassWord"/) }
+end
+
+describe file "/root/.mysql_secret" do
+  it { should_not exist }
+end
+
+describe command "env MYSQL_PWD=PassWord mysql -uroot -e 'SHOW DATABASES'" do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should eq "" }
+  its(:stdout) { should match(/^|\s+mysql\s+\|$/) }
 end
